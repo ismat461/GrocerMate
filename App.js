8d1976bccb0d292d8d56c7cd2bcf22f223576247
ismat461 <62961264+ismat461@users.noreply.github.com>
2023-03-29 18:06:32 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+//import * as React from 'react';
+import { Text, View, StyleSheet, Dimensions, Image, TouchableOpacity} from 'react-native';
+import Constants from 'expo-constants';
+import { NavigationContainer} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./Screens/HomeScreen";
+import NewList from "./Screens/NewList";
+import MyLists from "./Screens/MyLists";
+import MainList from "./Screens/MainList";
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{headerShown: false}}
+      >
+        <Stack.Screen
+          name="homescreen"
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          name="CreateNewList"
+          component={NewList}
+        />
+        <Stack.Screen
+          name="MyLists"
+          component={MyLists}
+        />
+        <Stack.Screen
+          name="MainList"
+          component={MainList}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
