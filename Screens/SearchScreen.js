4c64d3bdfc80a096154c55ebfe79d6aab2d17145
ismat461 <62961264+ismat461@users.noreply.github.com>
2023-03-31 00:25:59 +0000

    search screen updates

--- Screens/SearchScreen.js
+++ Screens/SearchScreen.js
@@ -40,20 +40,37 @@ export default function NewList({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.locationBar}>
-        <Image
-        // /workspaces/GrocerMate/assets/Icons/house-icon-gold.png
-          source={require('/workspaces/GrocerMate/assets/Icons/pink-map.png')}
-          style={styles.tinyIcon}
-        />
+      <View style={styles.pinkButtonContainer}>
+        <TouchableOpacity 
+        style={styles.pinkButton} onPress={() => navigation.navigate('MainList')}>
+          <Image
+            source={require('/workspaces/GrocerMate/assets/Icons/gold-back-sign.png')}
+            style={styles.largeIcon}
+          />
+        </TouchableOpacity>
 
-        <Text style={styles.paragraph}>Acme</Text>
+        <TouchableOpacity 
+        style={styles.pinkButton} onPress={() => navigation.navigate('homescreen')}
+        >
+          <Image
+          // /workspaces/GrocerMate/assets/Icons/house-icon-gold.png
+            source={require('/workspaces/GrocerMate/assets/Icons/house-icon-gold.png')}
+            style={styles.largeIcon}
+          />
+        </TouchableOpacity>
+      </View>
 
-        <Image
-          source={require('/workspaces/GrocerMate/assets/Icons/gold-search.png')}
-          style={styles.tinyIcon}
-        />
-      </TouchableOpacity>
+
+
+
+
+
+      
+
+    
+
+
+
       <TouchableOpacity
         onPress={() => navigation.navigate('homescreen')}
         style={styles.homeButton}>
@@ -66,18 +83,20 @@ export default function NewList({ navigation }) {
       <NameInput></NameInput>
 
 
-      <Text style={styles.labelText}>Your list is currently empty!</Text>
+      
 
       <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          onPress={() => navigation.navigate('SearchScreen')}
-          style={styles.button}>
-          <Text style={styles.buttonText}>Add Items</Text>
-          <Image
-            source={require('/workspaces/GrocerMate/assets/Icons/gold-add.png')}
-            style={styles.smallIcon}
-          />
-        </TouchableOpacity>
+
+        <Text style={styles.labelText}>SEARCH SCREEN</Text>
+      </View>
+
+      <View style={styles.grandTotalContainer}>
+        <Text style={styles.grandText}>
+          Grand Total:
+        </Text>
+        <Text style={styles.grandTextBold}>
+          $0.00
+        </Text>
       </View>
     </View>
   );
@@ -94,9 +113,13 @@ class NameInput extends Component {
       <View style={styles.textInputBox}>
         <TextInput
           style={styles.textInput}
-          placeholder="New List 1"
+          placeholder="Search Products"
           onChangeText={(text) => this.setState({ text })}
         />
+      <Image
+            source={require('/workspaces/GrocerMate/assets/Icons/gold-pencil.png')}
+            style={styles.smallIcon}
+      />
       </View>
     );
   }
@@ -111,37 +134,64 @@ orange: #F8D677
 pink-salmon: #F1785D
 */
 const styles = StyleSheet.create({
-  textInputBox: {
-    height: deviceHeight/10,
-    width: deviceHeight/3,
+  pinkButtonContainer: {
+    height: deviceHeight/15,
+    width: deviceWidth/.5,
+    //justifyContent: 'space-between',
     backgroundColor: '#285238',
-    //backgroundColor: '#F5F5DC',
-    fontSize: 20,
-    borderRadius: 30,
-    padding: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontFamily: 'Montserrat_400Regular',
-    left: 0,
-    right: 0,
-    top: 0,
+    justifyContent: 'space-evenly',
+    alignItems:'center',
+    top: -deviceHeight/6,
+    flexDirection: 'row'   
   },
-  grandTotalContainer: {
-    height: 40,
-    width: deviceWidth /3,
+  pinkButton: {
+    borderRadius: 15,
+    backgroundColor: '#F1785D',
+    height: deviceHeight/15,
+    width: deviceWidth/8,
+     
+    margin: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    //right: -deviceWidth/2.5,
+    //top: -deviceHeight/4.5,
+  },
+  //deviceHeight: 728.9
+  //deviceWidth: 392.72
+  largeIcon: {
+    height: deviceHeight/19,
+    width: deviceWidth/15,
+    resizeMode: 'contain',
+    justifyContent: 'center',
+  },
+  smallIcon: {
+    height: deviceHeight/20,
+    width: deviceWidth/20,
+    resizeMode: 'contain',
+    justifyContent: 'center',
+  },
+  textInputBox: {
+    height: deviceHeight/15,
+    width: deviceHeight/2.5,
+    //backgroundColor: '#285238',
     backgroundColor: '#F5F5DC',
     fontSize: 20,
     borderRadius: 30,
-    padding: 20,
     justifyContent: 'center',
     alignItems: 'center',
     fontFamily: 'Montserrat_400Regular',
-    top: -100,
+
+    top: -deviceHeight/5,
+    flexDirection: 'row',
   },
   textInput: {
     fontSize: 30,
+    height: deviceHeight/15,
+    width: deviceWidth/1.75,
+    textAlign: 'center',
     fontFamily: 'Montserrat_400Regular',
-    color: '#374B4A'
+    color: '#374B4A',
+    //backgroundColor: '#285238',
   },
   container: {
     flex: 1,
@@ -149,20 +199,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#B4CC9B',
   },
-  paragraph: {
-    margin: 10,
-    fontSize: 18,
-    textAlign: 'center',
-    color: '#374B4A',
-    fontFamily: 'Montserrat_400Regular',
-  },
+
   buttonContainer: {
     height: deviceHeight / 2.5,
-    width: deviceWidth / 3,
     left: 0,
     right: 0,
-    top: deviceHeight / 15,
+    top: -deviceHeight / 10,
     alignItems: 'center',
+    justifyContent: 'center'
   },
   button: {
     width: deviceWidth / 2,
@@ -175,49 +219,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     margin: 10,
   },
+
   buttonText: {
-    fontSize: 25,
+    fontSize: 30,
     color: '#F8D677',
     padding: 10,
   },
-  labelText: {
-    fontSize: 20,
-    color: '#F5F5DC',
-    alignItems: 'center',
-  },
-  smallIcon: {
-    height: 30,
-    width: 30,
-    resizeMode: 'contain',
-    justifyContent: 'center',
-  },
-  tinyIcon: {
-    height: 20,
-    width: 20,
-    resizeMode: 'contain',
-    justifyContent: 'center',
-  },
-  homeButton: {
-    borderRadius: 15,
-    backgroundColor: '#F1785D',
-    height: 40,
-    width: 40,
-    resizeMode: 'contain',
-    margin: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    right: -120,
-    top: -115,
-  },
-  locationBar: {
-    height: 30,
-    width: 140,
-    backgroundColor: '#F5F5DC',
-    borderRadius: 30,
-    left: -90,
-    top: -70,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-  },
+
+  
+  
 });
