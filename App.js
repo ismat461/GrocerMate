ab3879bf684975d8493cd8aad820e1a07fde7cba
ismat461 <62961264+ismat461@users.noreply.github.com>
2023-04-02 18:25:49 +0000

    TEST2

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import Checkout from "./Screens/Checkout";
 
 // /workspaces/GrocerMate/Screens/HomeScreen.js
 // TESTING
+// TESTING2
 const Stack = createNativeStackNavigator();
 
 export default function App() {
